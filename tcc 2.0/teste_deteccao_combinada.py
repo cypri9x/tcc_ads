f8f0552e0fa158c6fc4fa4faf0b9343f9b40aff4
Gustavo Cypriano <gustavo.de.carvalho.cypriano@gmail.com>
2023-08-14 19:20:11 -0300

    primeiro commit

---
+++ tcc 2.0/teste_deteccao_combinada.py
@@ -0,0 +1,24 @@
+import cv2
+
+classificador = cv2.CascadeClassifier('classificadores\haarcascade_frontalface_default.xml')
+
+
+imagem = cv2.imread('imagens\\pessoas2.jpg') 
+imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+
+
+cv2.waitKey()
+
+
+facesDetectadas = classificador.detectMultiScale(imagemCinza)
+
+
+print(len(facesDetectadas))
+
+print(facesDetectadas)
+
+for (x,y,l,a) in facesDetectadas:
+    cv2.rectangle(imagem, (x,y), (x+l, y + a), (0,255,255),2)
+
+cv2.imshow("Faces Encontradas", imagem)
+cv2.waitKey()
