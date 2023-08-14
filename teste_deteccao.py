61d0985d32b5563cc0fbb5ef098b025cd0af6296
Gustavo Cypriano <gustavo.de.carvalho.cypriano@gmail.com>
2023-08-14 20:21:46 -0300

    wtowu9qwo

--- teste_deteccao.py
+++ teste_deteccao.py
@@ -4,6 +4,8 @@ classificador = cv2.CascadeClassifier('./classificadores/haarcascade_frontalface
 
 imagem = cv2.imread('./imagens/pessoas2.jpg') 
 imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+width,height,s = imagem.shape
+
 
 
 cv2.waitKey()
@@ -16,9 +18,10 @@ print(len(facesDetectadas))
 
 print(facesDetectadas)
 
+
 for (x,y,l,a) in facesDetectadas:
     cv2.rectangle(imagem, (x,y), (x+l, y + a), (0,255,255),2)
-    print (x+l/2)
+    print (x+l/2-width/2)
 
 cv2.imshow("Faces Encontradas", imagem)
 cv2.waitKey()
