27a8673ae1204226803ddb85337d03b6724069aa
Gustavo Cypriano <gustavo.de.carvalho.cypriano@gmail.com>
2023-08-27 13:09:10 -0300

    ola

--- teste_deteccao.py
+++ teste_deteccao.py
@@ -1,12 +1,46 @@
 import cv2
+import time
+import RPi.GPIO as GPIO
+
 
 width = 640
 height = 480
+angle_x = 0
+angle_y = 0
+
 video = cv2.VideoCapture(0, cv2.CAP_V4L)
 video.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
 video.set(cv2.CAP_PROP_FRAME_WIDTH,width)
 classificador = cv2.CascadeClassifier('./classificadores/haarcascade_frontalface_default.xml')
 #classificadorOlhos = cv2.CascadeClassifier('./classificadores/haarcascade_eye.xml')
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(12,GPIO.OUT)
+pwm_x = GPIO.PWM(11,50)
+pwm_y = GPIO.PWM(12,50)
+
+pwm_x.start(0)
+pwm_y.start(0)
+
+
+def set_angle_x(v):
+    if v>0:
+        angle_x += 5
+    if angle_x > 360:
+        angle_x = 360 - angle_x
+    duty_cycle = (angle_x/18)+2
+    pwm_x.ChangeDutyCycle(duty_cycle)
+    time.sleep(0.5)
+
+def set_angle_y(v):
+    if v>0:
+        angle_y += 5
+    if angle_y > 360:
+        angle_y = 360 - angle_y
+    duty_cycle = (angle_y/18)+2
+    pwm_y.ChangeDutyCycle(duty_cycle)
+    time.sleep(0.5)
 
 
 while True:
@@ -17,8 +51,12 @@ while True:
     if len(facesDetectadas) > 0:
         x,y,l,a = facesDetectadas[0]
         centro_x = x + l // 2
-        lado_movimento = centro_x - width // 2
-        print(lado_movimento)
+        centro_y = y + a // 2
+        lado_movimento_x = centro_x - width // 2
+        lado_movimento_y = centro_y - height // 2
+        set_angle_x(lado_movimento_x)
+        set_angle_y(lado_movimento_y)
+        print(lado_movimento_x,",",lado_movimento_y)
         cv2.rectangle(imagem, (x, y), (x + l, y + a), (0, 255, 255), 2)
     
     #for (x, y, l, a) in facesDetectadas:
