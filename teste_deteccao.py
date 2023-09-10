3028a4d50e8fd7b20e6822650670d2acf5b2a0b9
Gustavo Cypriano <gustavo.de.carvalho.cypriano@gmail.com>
2023-09-10 16:36:19 -0300

    adadadadadadad

--- teste_deteccao.py
+++ teste_deteccao.py
@@ -48,10 +48,10 @@ def set_angle_x(angle):
         time.sleep(1)     
 
 def teste_loop():
-    for dc in range(0, 51, 5):
+    for dc in range(0, 21, 5):
         pwm_x.ChangeDutyCycle(dc)
         time.sleep(0.1)
-    for dc in range(50, -1, -5):
+    for dc in range(20, -1, -5):
         pwm_x.ChangeDutyCycle(dc)
         time.sleep(0.1)
 
