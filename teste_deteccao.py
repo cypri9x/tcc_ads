9f554b98abccfdde46202d55af344eb2dc131e0c
Gustavo Cypriano <gustavo.de.carvalho.cypriano@gmail.com>
2023-09-10 16:07:19 -0300

    sdadadasdadada

--- teste_deteccao.py
+++ teste_deteccao.py
@@ -54,7 +54,7 @@ def teste_loop():
     pwm_x.ChangeDutyCycle(angle_x)  # Configurar o ângulo do motor
     time.sleep(1)
     pwm_x.ChangeDutyCycle(0)
-    time.slee(0.5)
+    time.sleep(0.5)
 
 
 while True:
