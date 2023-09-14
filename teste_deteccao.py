ef0fba51f58d6b6eeca9caa481b9b04d1e33e4fa
Robinson Carvalho <decarvalhorobinson@gmail.com>
2023-09-14 18:27:31 -0300

    asdf

--- teste_deteccao.py
+++ teste_deteccao.py
@@ -3,7 +3,7 @@ from gpiozero import AngularServo
 import time
 
 # Conecte o servo angular à porta GPIO 17 (ou outra porta de sua escolha)
-servo = AngularServo(11, min_angle=0, max_angle=180, initial_angle=90, min_pulse_width=0.5/1000.0, max_pulse_width=2.5/1000.0)
+servo = AngularServo(18, min_angle=0, max_angle=180, initial_angle=90, min_pulse_width=0.5/1000.0, max_pulse_width=2.5/1000.0)
 
 # Ajuste para um movimento mais rápido e suave
 step = 5
@@ -12,6 +12,7 @@ delay = 0.01
 width = 640
 height = 480
 move_x = 90
+move_y = 90
 
 video = cv2.VideoCapture(0, cv2.CAP_V4L)
 video.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
@@ -37,6 +38,24 @@ def set_angle_x(angle):
     servo.angle = move_x
     time.sleep(0.01)
 
+def set_angle_y(angle):
+    global move_y
+    if 0.4 <= angle <= 0.6:
+        return
+    angle = (2 * angle) - 1
+    angle = angle * 10
+    
+    move_y += int(angle)
+    if move_y >= 180 or move_y <= 0:
+        move_y = 180
+        return
+    elif move_y <= 0:
+        move_y = 0  
+        return
+
+    servo.angle = move_y
+    time.sleep(0.01)
+
 while True:
     conectado, imagem = video.read()
     imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
@@ -48,6 +67,8 @@ while True:
         centro_y = y + a // 2
         lado_movimento_x = centro_x - width // 2
         lado_movimento_x = float(lado_movimento_x) / float(width)
+        lado_movimento_y = centro_y - height // 2
+        lado_movimento_y = float(lado_movimento_y) / float(height)
         set_angle_x(lado_movimento_x)
 
         print(lado_movimento_x)
